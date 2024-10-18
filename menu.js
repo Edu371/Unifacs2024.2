627fca71c6cc788804bcd617b66a08cfe83ad881
Edu371 <smartedu28@gmail.com>
2024-10-17 21:35:27 -0300

    updata

--- menu.js
+++ menu.js
@@ -2,12 +2,19 @@ var credits_dialog
 document.addEventListener("DOMContentLoaded", function(e) {
     credits_dialog = document.getElementsByClassName("credits_dialog")[0];
     console.log(credits_dialog)
+    credits_dialog.addEventListener("click", onClick);
 })
 
 function show_credits() {
-    credits_dialog.show()
+    credits_dialog.showModal()
 }
 
 function close_credits() {
     credits_dialog.close()
+}
+
+function onClick(event) {
+  if (event.target === credits_dialog) {
+    credits_dialog.close();
+  }
 }
