87dab32a620a6bd06de18ebfffc1df7995b3f21b
Edu371 <smartedu28@gmail.com>
2024-10-24 21:59:49 -0300

    update

--- menu.js
+++ menu.js
@@ -1,20 +1,41 @@
+const { style } = document.documentElement;
+
+const fonts = ["Pixel", "Comic Sans MS"];
+var current_font = 0;
+
+var options_dialog
 var credits_dialog
 document.addEventListener("DOMContentLoaded", function(e) {
+    options_dialog = document.getElementsByClassName("options_dialog")[0];
+    options_dialog.addEventListener("click", closeDialog);
     credits_dialog = document.getElementsByClassName("credits_dialog")[0];
-    console.log(credits_dialog)
-    credits_dialog.addEventListener("click", onClick);
+    credits_dialog.addEventListener("click", closeDialog);
 })
 
+function show_options() {
+    options_dialog.showModal();
+}
+
+function close_options() {
+    options_dialog.close();
+}
+
+function change_font() {
+    current_font += 1;
+    if (current_font > fonts.length-1) {current_font = 0}
+    style.setProperty('--font', fonts[current_font])
+}
+
 function show_credits() {
-    credits_dialog.showModal()
+    credits_dialog.showModal();
 }
 
 function close_credits() {
-    credits_dialog.close()
+    credits_dialog.close();
 }
 
-function onClick(event) {
-  if (event.target === credits_dialog) {
-    credits_dialog.close();
-  }
+function closeDialog(event) {
+    if (event.target.nodeName === "DIALOG") {
+        event.target.close();
+    }
 }
