330b305a9d8bd54909757166227f9bd7ad953c98
Edu371 <smartedu28@gmail.com>
2024-10-27 16:43:17 -0300

    update

---
+++ base.js
@@ -0,0 +1,12 @@
+const { style } = document.documentElement;
+
+const fonts = [["Pixel", "normal"], ["Comic Sans MS", "bold"]];
+var current_font = 0;
+if (sessionStorage.getItem("current_font") == null) {
+    sessionStorage.setItem("current_font", current_font);
+} else {
+    current_font = Number(sessionStorage.getItem("current_font"));
+}
+
+style.setProperty('--font', fonts[current_font][0])
+style.setProperty('--font-weight', fonts[current_font][1])
