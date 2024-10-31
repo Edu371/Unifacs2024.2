d58d0daab901b630d3d756111e7ef6c0f6e558a5
Edu371 <smartedu28@gmail.com>
2024-10-30 22:03:30 -0300

    ajeitar tamanho das images

--- jogo.js
+++ jogo.js
@@ -2,16 +2,23 @@ input_tipo = 1
 images = input_tipo == 1 ? ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png"] : ["assets/1.png"]
 
 var imagesContainer;
+var imagesContainer2;
 document.addEventListener("DOMContentLoaded", function(e) {
     imagesContainer = document.getElementById('images');
+    imagesContainer2 = document.getElementById('images2');
     if (input_tipo == 0) {
         imagesContainer.className = 'uma_imagem'
     } else {
         imagesContainer.className = 'quatro_imagem'
+        imagesContainer2.className = 'quatro_imagem'
     }
-    images.forEach((img) => {
+    for (var i=0; i < images.length; i++) {
         const imageElement = document.createElement('img');
-        imageElement.src = img;
-        imagesContainer.appendChild(imageElement);
-    });
+        imageElement.src = images[i];
+        if (i < 2) {
+            imagesContainer.appendChild(imageElement);
+        } else {
+            imagesContainer2.appendChild(imageElement);
+        }
+    };
 })
