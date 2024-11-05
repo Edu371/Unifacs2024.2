42aef5bced060b4acb290b73224b8b1009f67b4c
Edu371 <smartedu28@gmail.com>
2024-11-04 21:16:04 -0300

    jogabilidade

--- jogo.js
+++ jogo.js
@@ -1,12 +1,73 @@
-input_tipo = 1
-images = input_tipo == 1 ? ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png"] : ["assets/1.png"]
-resposta_tipo = 0;
-if (resposta_tipo == 0) {
-    respostas = ["Opção1", "Opção2", "Opção3", "Opção4"];
-} else {
-    respostas = ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png"];
+function teste() {
+    correto = 0;
+    input_tipo = 0;
+    images = input_tipo == 1 ? ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png"] : ["assets/1.png"];
+    resposta_tipo = 0;
+    if (resposta_tipo == 0) {
+        respostas = ["Opção1", "Opção2", "Opção3", "Opção4"];
+    } else {
+        respostas = ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png"];
+    }
+    descricao = "AAAAAAA";
 }
-descricao = "AAAAAAA";
+
+// input: uma_imagem
+// output: nome categoria
+function modo1() {
+    input_tipo = 0;
+    resposta_tipo = 0;
+    descricao = "Responda qual é a Categoria"
+    
+    cat = random_category();
+    item = random_item(cat);
+    respostas = random_category3(cat);
+    correto = random(4);
+    respostas.splice(correto, 0, cat);
+
+    images = ["database/"+cat+"/"+item+"/"+String(random(db[cat][item])).padStart(3, '0')+".png"];
+}
+// input: uma_imagem
+// output: nome objeto
+function modo2() {
+    
+}
+
+// input: quatro_imagem
+// output: nome categoria
+function modo3() {
+    
+}
+
+// input: uma_imagem
+// output: imagem do mesmo objeto
+function modo4() {
+    
+}
+
+// input: uma_imagem
+// output: imagem da mesmo categoria
+function modo5() {
+    
+}
+
+// input: quatro_imagem
+// output: imagem da mesmo categoria
+function modo6() {
+    
+}
+
+// String(n).padStart(3, '0')
+
+//teste();
+modo1();
+
+function selecionar(n) {
+    if (n == correto) {
+        window.alert("acertou");
+    } else {
+        window.alert("errado");
+    }
+}
 
 var imagesContainer;
 var imagesContainer2;
@@ -38,6 +99,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     for (var i=0; i < respostas.length; i++) {
         const button = document.createElement('button');
         button.className = "btn-visualmente";
+        button.setAttribute("onClick","selecionar(" + i + ")");
 
         if (resposta_tipo == 0) {
             button.innerHTML = respostas[i];
